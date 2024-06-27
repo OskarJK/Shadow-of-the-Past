286f4dd462aa6358253e5aedd7259ddb31ee3d55
OskarJK <106971135+OskarJK@users.noreply.github.com>
2024-06-27 16:38:44 +0200

    Change

--- MainWindow.py
+++ MainWindow.py
@@ -2,7 +2,7 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1380, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
